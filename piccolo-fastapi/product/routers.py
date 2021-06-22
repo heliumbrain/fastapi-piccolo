5e24799816376c25718ad6b3c78c5c3aaa6d95a6
heliumbrain <nils@kanevad.se>
2021-06-22 15:26:29 +0200

    Trying to fix the dependencies for session_auth

--- piccolo-fastapi/product/routers.py
+++ piccolo-fastapi/product/routers.py
@@ -1,13 +1,21 @@
+from http.client import HTTPConnection
 from typing import List
+from fastapi import Depends, Request
 
 from fastapi.routing import APIRouter
 
 from .models import PydanticProductIn, PydanticProductOut
 from .tables import Product
+from piccolo.apps.user.tables import BaseUser
+from piccolo_api.session_auth.middleware import SessionsAuthBackend
 
 # Create a product router that can later be included in FastAPI
 product_router = APIRouter()
 
+session_auth = SessionsAuthBackend(admin_only=True)
+
+async def session_admin():
+    await session_auth.authenticate() # TODO: HOW TO FIX THIS SHIT?
 
 """
 Defining the routes for the product_router.
@@ -21,7 +29,7 @@ async def get_products():
 
 
 @product_router.post("/products/", response_model=PydanticProductOut)
-async def create_product(product: PydanticProductIn):
+async def create_product(product: PydanticProductIn, user: BaseUser = Depends(session_admin)):
 
     product = Product(**product.__dict__)
     await product.save().run()
